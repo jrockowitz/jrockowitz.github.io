1f1b5329b6a72b29e2e136d1ab936d697d92fbad
jrockowitz <jrockowitz@gmail.com>
2015-10-09 05:44:45 -0400

    Initial setup of Grunt, Bower, jQuery, and Bootstrap.

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+
+    grunt.initConfig({
+        // Get project configuration from package.json.
+        pkg: require('./package.json'),
+        concat: {
+            dist: {
+                src: [
+                    'bower_components/jquery/jquery.min.js',
+                    'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.min.js'
+                ],
+                dest: 'assets/js/vendor.min.js'
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'js/main.min.js': 'js/main.js'
+                }
+            },
+        },
+        exec: {
+            build: {
+                cmd: 'jekyll build'
+            },
+            serve: {
+                // http://unix.stackexchange.com/questions/102956/how-to-run-a-command-in-the-background-with-a-delay
+                cmd: '(sleep 5; open http://localhost:4000) &; jekyll serve --drafts --watch'
+            },
+            deploy: {
+                cmd: 'echo "TODO: Commit and push changes to GitHub'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-exec');
+
+    grunt.registerTask('default', [ 'uglify', 'concat', 'exec:build' ]);
+    grunt.registerTask('serve', [ 'uglify', 'concat', 'exec:serve' ]);
+    grunt.registerTask('deploy', [ 'default', 'exec:deploy' ]);
+
+};
